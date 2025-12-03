55216aa14eb7ade5ad4ae4c0885d5f09e83b1791
ManosKTR <77736170+ManosKTR@users.noreply.github.com>
2025-12-03 18:58:24 +0200

    Customer.java

--- src/main/java/models/Customer.java
+++ src/main/java/models/Customer.java
@@ -11,16 +11,16 @@ package models;
 public class Customer extends User {
 
     private final ShoppingCart cart;
-
+// [Katramados] Calls super with CUSTOMER role, Finalized Nov. 30th
     public Customer(String username, String password) {
         super(username, password, Role.CUSTOMER);
         this.cart = new ShoppingCart(this);
     }
-
+// [Katramados] Returns owned cart instance, Finalized Nov. 30th
     public ShoppingCart getCart() {
         return cart;
     }
-
+// [Katramados] Customer-specific string output, Finalized Nov. 30th
     @Override
     public String toString() {
         return "Customer{id=" + getID() +
